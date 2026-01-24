56d322779812592fd6db610dbd4bc633a54ea49c
Asianzu Henry <asianzuhenry@gmail.com>
2026-01-24 13:03:30 +0300

    fix typo

--- services/web/src/data/users.ts
+++ services/web/src/data/users.ts
@@ -3,14 +3,14 @@ import type { User } from "../types/basetypes";
 const users: User[] = [
     {
         id: 1,
-        username: "john_doe",
+        name: "john_doe",
         email: "john.doe@example.com",
         accountType: "buyer",
         phoneNumber: "123-456-7890",
     },
     {
         id: 2,
-        username: "jane_seller",
+        name: "jane_seller",
         email: "jane.seller@example.com",
         accountType: "seller",
         phoneNumber: "098-765-4321",
